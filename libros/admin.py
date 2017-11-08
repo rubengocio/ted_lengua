4fb1b2f5ebf0110c30b0441097b01d7b4e83199f
Ruben Gocio <rubengocio@gmail.com>
2017-11-07 23:15:37 -0300

    modelo opciones

--- libros/admin.py
+++ libros/admin.py
@@ -1,22 +1,36 @@
 from django.contrib import admin
-from .models import Categoria, Imagen, Libro, Capitulo, Comentario, Contenido
+from .models import Categoria, Imagen, Libro, Capitulo, Comentario, Opcion, Contenido
+
+# , Autoevaluacion, Opcion
+
 
 class LibroAdmin(admin.ModelAdmin):
     list_display = ('id', 'titulo', 'categoria')
     list_filter = ('categoria','usuario')
 
+
 class CapituloAdmin(admin.ModelAdmin):
-    list_display = ('id' ,'libro', 'titulo', 'orden')
+    list_display = ('id', 'libro', 'titulo', 'orden')
+
 
 class ImagenAdmin(admin.ModelAdmin):
     list_display = ('id', 'titulo', 'categoria')
     list_filter = ('categoria', )
 
+
+class OpcionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'texto', 'opcion', 'categoria')
+    list_filter = ('categoria', )
+
+
+
 # Register your models here.
+
 admin.site.register(Categoria)
 admin.site.register(Imagen, ImagenAdmin)
 admin.site.register(Libro, LibroAdmin)
 admin.site.register(Capitulo, CapituloAdmin)
 admin.site.register(Comentario)
 admin.site.register(Contenido)
+admin.site.register(Opcion, OpcionAdmin)
 
--- libros/models.py
+++ libros/models.py
@@ -111,3 +111,36 @@ class Contenido(models.Model):
     usuario = models.ForeignKey(User)
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
+
+
+PUNTAJE = ((0, '0'), (1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5'))
+
+
+class Opcion(models.Model):
+    categoria = models.ForeignKey(Categoria, null=True, blank=True, default=None)
+    opcion = models.ForeignKey('Opcion', null=True, blank=True, default=None, related_name='opcion_padre')
+    texto = models.CharField(max_length=256)
+    puntaje = models.PositiveIntegerField(default=0, choices=PUNTAJE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '%s' % self.texto
+
+    def __unicode__(self):
+        return '%s' % self.texto
+
+    def get_hijas(self):
+        return Opcion.objects.filter(opcion=self).order_by('id')
+
+    @staticmethod
+    def get_opciones(categoria):
+        return Opcion.objects.filter(categoria=categoria).order_by('id')
+
+
+class Autoevaluacion(models.Model):
+    opcion = models.ForeignKey(Opcion)
+    usuario = models.ForeignKey(User)
+    puntaje = models.PositiveIntegerField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
